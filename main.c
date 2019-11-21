3ca055b8875f5449d03c7450500f333d4f9d57c4
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-11-21 15:21:07 +0900

    05

--- main.c
+++ main.c
@@ -5,10 +5,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	char str[30]="happy C programming";
-	
-	
-	printf("string lenth \" %s \" : %i", str, strlen(str));
-	
+	int i;
+	char input[100];
+	FILE *fp;
 
+
+	fp = fopen("sample.txt", "w");
+	for(i=0; i<3; i++)
+	{
+		printf("input a word: ");
+		scanf("%s", input);
+		fprintf(fp, "%s\n", input);		
+	}
+
+	fclose(fp);	
+	
+	return 0;
 }
